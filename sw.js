16bb7298533f9f8928111f27bd0eff26f563e19e
xiaofeiwangluo <925819523@qq.com>
2026-09-09 23:40:45 +0800

    deploy sw.js (wob same as weld: save/speed/direction v37)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker - 离线缓存
-const CACHE_NAME = 'fiber-laser-calc-v77';
+﻿// Service Worker - 绂荤嚎缂撳瓨
+const CACHE_NAME = 'fiber-laser-calc-v78';
 const ASSETS = [
   './',
   './index.html',
